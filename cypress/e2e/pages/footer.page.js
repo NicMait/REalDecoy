d9542eaf02dcdcd2a20ea7953406f0e55253ffc9
Nicole Maitland <nicolemtlnd@gmail.com>
2023-03-31 16:25:33 -0500

    Added Logout Test Scenarios

--- cypress/e2e/pages/footer.page.js
+++ cypress/e2e/pages/footer.page.js
@@ -3,11 +3,12 @@ class Footer {
 
 	get twitterLink () { return('a[href="https://twitter.com/saucelabs"]')}
 	get facebookLink () { return ('a[href="https://www.facebook.com/saucelabs"]')}
-	get linkedLink () { return ('a[href="https://www.linkedin.com/company/sauce-labs/"]')}
+	get linkedLink () { return ('.social_linkedin')}
 
 
 
 	ConfirmFooter(){
+		cy.get('.footer').scrollIntoView()
 		cy.get(this.twitterLink).should('be.visible')
 		cy.get(this.facebookLink).should('be.visible')
 		cy.get(this.linkedLink).should('be.visible')
